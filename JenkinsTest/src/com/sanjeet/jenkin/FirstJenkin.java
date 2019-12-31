e22340f8b5abfcb8b9a08910fb1328e3b3243d51
kapilasthana2008 <kapilasthana2008@gmail.com>
2019-12-31 12:39:18 +0530

    aaa

--- JenkinsTest/src/com/sanjeet/jenkin/FirstJenkin.java
+++ JenkinsTest/src/com/sanjeet/jenkin/FirstJenkin.java
@@ -1,7 +1,11 @@
 package com.sanjeet.jenkin;
 class Test{
 	public static  int sum(int a ,int b) {
+		System.out.println(a+b);
 		return a+b;
+	}
+	public static int mul(int a,int b) {
+		return a*b;
 	}
 }
 public class FirstJenkin {
@@ -9,5 +13,7 @@ public class FirstJenkin {
 public static void main(String[] args) {
 	int sum=Test.sum(2, 3);
 	System.out.println(sum);
+	int mul=Test.mul(2, 3);
+	System.out.println(mul);
 }
 }
